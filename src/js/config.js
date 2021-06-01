f209c69d7647d69365dc08a2dbff9f98ce29ede2
HP <akhimunna2000@gmail.com>
2021-06-01 22:42:28 +0530

    my commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '79aed492-93dc-4f5d-9a9f-c758368cae08';
+export const MODAL_CLOSE_SEC = 2.5;
